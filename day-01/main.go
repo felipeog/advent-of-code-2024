24ffa20a7fc238e9ae2ab97809da7cd3e5d72eb2
felipeog <17603069+felipeog@users.noreply.github.com>
2024-12-02 21:59:49 -0300

    add day 1

---
+++ day-01/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	firstHalf()
+	secondtHalf()
+}
+
+func firstHalf() {
+	// file, _ := os.Open("sample.txt")
+	file, _ := os.Open("input.txt")
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	sum := 0
+
+	leftList := []int{}
+	rightList := []int{}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.SplitN(line, "   ", 2)
+
+		leftValue, _ := strconv.Atoi(parts[0])
+		rightValue, _ := strconv.Atoi(parts[1])
+
+		leftList = append(leftList, leftValue)
+		rightList = append(rightList, rightValue)
+	}
+
+	sort.Ints(leftList)
+	sort.Ints(rightList)
+
+	absInt := func(n int) int {
+		if n < 0 {
+			return -n
+		}
+		return n
+	}
+
+	for index := range leftList {
+		sum = sum + absInt(leftList[index]-rightList[index])
+	}
+
+	fmt.Println("firstHalf", sum)
+}
+
+func secondtHalf() {
+	// file, _ := os.Open("sample.txt")
+	file, _ := os.Open("input.txt")
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	sum := 0
+
+	leftList := []int{}
+	rightList := []int{}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.SplitN(line, "   ", 2)
+
+		leftValue, _ := strconv.Atoi(parts[0])
+		rightValue, _ := strconv.Atoi(parts[1])
+
+		leftList = append(leftList, leftValue)
+		rightList = append(rightList, rightValue)
+	}
+
+	rightListMap := make(map[int]int)
+	for index := range rightList {
+		mapKey := rightList[index]
+		count, exists := rightListMap[mapKey]
+
+		if exists {
+			rightListMap[mapKey] = count + 1
+		} else {
+			rightListMap[mapKey] = 1
+		}
+	}
+
+	for index := range leftList {
+		leftItem := leftList[index]
+		rightCount, exists := rightListMap[leftItem]
+
+		if exists {
+			sum = sum + leftItem*rightCount
+		}
+	}
+
+	fmt.Println("secondHalf", sum)
+}
