67083479747e14eed1768d07e6b2d169be801405
felipeog <17603069+felipeog@users.noreply.github.com>
2024-12-04 20:24:33 -0300

    day 4 cleanup

--- day04/day04.go
+++ day04/day04.go
@@ -17,79 +17,45 @@ func FirstHalf() int {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		text := scanner.Text()
-		substrings := strings.SplitN(text, "", -1)
+		substrings := strings.Split(text, "")
 		matrix = append(matrix, substrings)
+	}
+
+	directions := []struct {
+		rowStep, colStep int
+	}{
+		{-1, 0},  // up
+		{-1, 1},  // up right
+		{0, 1},   // right
+		{1, 1},   // down right
+		{1, 0},   // down
+		{1, -1},  // down left
+		{0, -1},  // left
+		{-1, -1}, // up left
 	}
 
 	count := 0
 	for rowIndex, row := range matrix {
 		for colIndex := range row {
-			letter := matrix[rowIndex][colIndex]
-			var word string
-
-			if letter != "X" {
+			if matrix[rowIndex][colIndex] != "X" {
 				continue
 			}
 
-			hasTop := rowIndex-3 >= 0
-			hasRight := colIndex+3 < len(matrix[0])
-			hasBottom := rowIndex+3 < len(matrix)
-			hasLeft := colIndex-3 >= 0
+			for _, direction := range directions {
+				valid := true
+				for letterIndex := range len(xmas) {
+					newRowIndex := rowIndex + letterIndex*direction.rowStep
+					newColIndex := colIndex + letterIndex*direction.colStep
+					inside := newRowIndex >= 0 && newRowIndex < len(matrix) && newColIndex >= 0 && newColIndex < len(matrix[0])
 
-			// TODO: improve this, too many `if`s
-			if hasTop {
-				word = letter + matrix[rowIndex-1][colIndex] + matrix[rowIndex-2][colIndex] + matrix[rowIndex-3][colIndex]
-				if word == xmas {
-					count += 1
+					if !inside || matrix[newRowIndex][newColIndex] != string(xmas[letterIndex]) {
+						valid = false
+						break
+					}
 				}
-			}
 
-			if hasTop && hasRight {
-				word = letter + matrix[rowIndex-1][colIndex+1] + matrix[rowIndex-2][colIndex+2] + matrix[rowIndex-3][colIndex+3]
-				if word == xmas {
-					count += 1
-				}
-			}
-
-			if hasRight {
-				word = letter + matrix[rowIndex][colIndex+1] + matrix[rowIndex][colIndex+2] + matrix[rowIndex][colIndex+3]
-				if word == xmas {
-					count += 1
-				}
-			}
-
-			if hasBottom && hasRight {
-				word = letter + matrix[rowIndex+1][colIndex+1] + matrix[rowIndex+2][colIndex+2] + matrix[rowIndex+3][colIndex+3]
-				if word == xmas {
-					count += 1
-				}
-			}
-
-			if hasBottom {
-				word = letter + matrix[rowIndex+1][colIndex] + matrix[rowIndex+2][colIndex] + matrix[rowIndex+3][colIndex]
-				if word == xmas {
-					count += 1
-				}
-			}
-
-			if hasBottom && hasLeft {
-				word = letter + matrix[rowIndex+1][colIndex-1] + matrix[rowIndex+2][colIndex-2] + matrix[rowIndex+3][colIndex-3]
-				if word == xmas {
-					count += 1
-				}
-			}
-
-			if hasLeft {
-				word = letter + matrix[rowIndex][colIndex-1] + matrix[rowIndex][colIndex-2] + matrix[rowIndex][colIndex-3]
-				if word == xmas {
-					count += 1
-				}
-			}
-
-			if hasTop && hasLeft {
-				word = letter + matrix[rowIndex-1][colIndex-1] + matrix[rowIndex-2][colIndex-2] + matrix[rowIndex-3][colIndex-3]
-				if word == xmas {
-					count += 1
+				if valid {
+					count++
 				}
 			}
 		}
@@ -110,7 +76,7 @@ func SecondHalf() int {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		text := scanner.Text()
-		substrings := strings.SplitN(text, "", -1)
+		substrings := strings.Split(text, "")
 		matrix = append(matrix, substrings)
 	}
 
@@ -134,9 +100,9 @@ func SecondHalf() int {
 				diagonal1 = matrix[rowIndex-1][colIndex-1] + letter + matrix[rowIndex+1][colIndex+1]
 				diagonal2 = matrix[rowIndex-1][colIndex+1] + letter + matrix[rowIndex+1][colIndex-1]
 
-				// TODO: improve string matching
+				// TODO: improve string matching?
 				if (diagonal1 == sam || diagonal1 == mas) && (diagonal2 == sam || diagonal2 == mas) {
-					count += 1
+					count++
 				}
 			}
 		}
