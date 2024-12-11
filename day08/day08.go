f57f61c4c7bbf8e6067b9caa5518706c6801fe86
felipeog <17603069+felipeog@users.noreply.github.com>
2024-12-11 15:17:33 -0300

    add day 8 second half

--- day08/day08.go
+++ day08/day08.go
@@ -62,7 +62,66 @@ func FirstHalf() int {
 	return len(antinodeMap)
 }
 
-// TODO:
 func SecondHalf() int {
-	return -1
+	type coord struct {
+		row int
+		col int
+	}
+
+	// file, _ := os.Open("sample.txt")
+	file, _ := os.Open("input.txt")
+	scanner := bufio.NewScanner(file)
+	antennaRegex := regexp.MustCompile("[0-9a-zA-Z]")
+	antennaMap := map[string][]coord{}
+	antinodeMap := map[coord]bool{}
+	rowCount := 0
+	colCount := 0
+
+	for scanner.Scan() {
+		text := scanner.Text()
+		positions := antennaRegex.FindAllStringIndex(text, -1)
+
+		for _, position := range positions {
+			antenna := text[position[0]:position[1]]
+			antennaMap[antenna] = append(antennaMap[antenna], coord{rowCount, position[0]})
+		}
+
+		if rowCount == 0 {
+			colCount = len(text)
+		}
+
+		rowCount++
+	}
+
+	for _, coords := range antennaMap {
+		for i := range coords {
+			from := coords[i]
+
+			if len(coords) > 1 {
+				antinodeMap[from] = true
+			}
+
+			for j := range coords {
+				if j == i {
+					continue
+				}
+
+				to := coords[j]
+				distance := coord{to.row - from.row, to.col - from.col}
+
+				for {
+					antinode := coord{to.row + distance.row, to.col + distance.col}
+
+					if antinode.row < 0 || antinode.row >= rowCount || antinode.col < 0 || antinode.col >= colCount {
+						break
+					}
+
+					antinodeMap[antinode] = true
+					to = antinode
+				}
+			}
+		}
+	}
+
+	return len(antinodeMap)
 }
--- day08/day08_test.go
+++ day08/day08_test.go
@@ -11,9 +11,8 @@ func TestFirstHalf(t *testing.T) {
 	}
 }
 
-// TODO:
 func TestSecondHalf(t *testing.T) {
-	expected := 0
+	expected := 1280
 	actual := SecondHalf()
 
 	if expected != actual {
