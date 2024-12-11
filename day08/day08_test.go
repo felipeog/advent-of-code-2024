040f4b1505288cea96778e507ab79c2479247e41
felipeog <17603069+felipeog@users.noreply.github.com>
2024-12-11 13:23:38 -0300

    add day 8 boilerplate

---
+++ day08/day08_test.go
@@ -0,0 +1,23 @@
+package day08
+
+import "testing"
+
+// TODO:
+func TestFirstHalf(t *testing.T) {
+	expected := 0
+	actual := FirstHalf()
+
+	if expected != actual {
+		t.Errorf("TestFirstHalf() = %v; want %v", actual, expected)
+	}
+}
+
+// TODO:
+func TestSecondHalf(t *testing.T) {
+	expected := 0
+	actual := SecondHalf()
+
+	if expected != actual {
+		t.Errorf("TestSecondHalf() = %v; want %v", actual, expected)
+	}
+}
