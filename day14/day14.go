40c76cf3755531b21f3f7fd579762ded6e20eeef
felipeog <17603069+felipeog@users.noreply.github.com>
2024-12-15 05:24:00 -0300

    add day 14 boilerplate

---
+++ day14/day14.go
@@ -0,0 +1,11 @@
+package day14
+
+// TODO:
+func FirstHalf() int {
+	return -1
+}
+
+// TODO:
+func SecondHalf() int {
+	return -1
+}
---
+++ day14/readme.md
@@ -0,0 +1,3 @@
+<!-- TODO: -->
+
+# [Day 14](https://adventofcode.com/2024/day/14)
