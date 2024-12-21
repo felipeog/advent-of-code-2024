68c756b4c1444fdc3b6458b9977693329d6dd993
felipeog <17603069+felipeog@users.noreply.github.com>
2024-12-21 10:22:30 -0300

    add day 18 second half

--- day18/day18.go
+++ day18/day18.go
@@ -2,6 +2,7 @@ package day18
 
 import (
 	"bufio"
+	"fmt"
 	"math"
 	"os"
 	"strconv"
@@ -123,7 +124,133 @@ func FirstHalf() int {
 	return minMoves
 }
 
-// TODO:
-func SecondHalf() int {
-	return -1
+func SecondHalf() string {
+	type coord struct {
+		row int
+		col int
+	}
+
+	const (
+		up    = 0
+		right = 1
+		down  = 2
+		left  = 3
+	)
+
+	rowCount := 71
+	colCount := 71
+	start := coord{0, 0}
+	end := coord{rowCount - 1, colCount - 1}
+	stepMap := map[int]coord{
+		up:    {-1, 0},
+		right: {0, 1},
+		down:  {1, 0},
+		left:  {0, -1},
+	}
+
+	var minMoves, pathCost int
+	var hasSolution bool
+	var result coord
+	corruptedMap := make(map[coord]bool)
+	costMap := make(map[coord]int)
+	visitedMap := make(map[coord]int)
+
+	var traverse func(currCoord coord, direction int)
+	traverse = func(currCoord coord, direction int) {
+		// base case
+		if currCoord == end {
+			hasSolution = true
+			if pathCost < minMoves {
+				minMoves = pathCost
+			}
+			return
+		}
+
+		// skip if it isn't cheaper
+		if pathCost >= minMoves {
+			return
+		}
+
+		from := direction - 1
+		if from < 0 {
+			from += 4
+		}
+		to := direction + 1
+		if to > 3 {
+			to -= 4
+		}
+		if from > to {
+			from, to = to, from
+		}
+
+		nextDirections := []int{direction, from, to}
+		for _, nextDirection := range nextDirections {
+			step := stepMap[nextDirection]
+			nextCoord := coord{currCoord.row + step.row, currCoord.col + step.col}
+
+			// skip if it isn't cheaper
+			if prevCost, exists := costMap[nextCoord]; exists && pathCost >= prevCost {
+				continue
+			}
+			costMap[nextCoord] = pathCost
+
+			// skip if it's invalid
+			if nextCoord.row < 0 || nextCoord.row >= rowCount || nextCoord.col < 0 || nextCoord.col >= colCount {
+				continue
+			}
+			if _, exists := corruptedMap[nextCoord]; exists {
+				continue
+			}
+			if _, exists := visitedMap[nextCoord]; exists {
+				continue
+			}
+
+			// make move
+			visitedMap[nextCoord] = nextDirection
+			pathCost++
+
+			// recurse
+			traverse(nextCoord, nextDirection)
+
+			// backtrack
+			delete(visitedMap, nextCoord)
+			pathCost--
+		}
+	}
+
+	// file, _ := os.Open("sample.txt")
+	file, _ := os.Open("input.txt")
+	scanner := bufio.NewScanner(file)
+	lineCount := 0
+
+	for scanner.Scan() {
+		text := scanner.Text()
+		substrings := strings.Split(text, ",")
+		row, _ := strconv.Atoi(substrings[1])
+		col, _ := strconv.Atoi(substrings[0])
+		currCoord := coord{row, col}
+		corruptedMap[currCoord] = true
+
+		if lineCount < 1025 {
+			lineCount++
+			continue
+		}
+
+		minMoves = math.MaxInt
+		costMap = map[coord]int{}
+		visitedMap = map[coord]int{}
+		pathCost = 0
+		hasSolution = false
+
+		traverse(start, down)
+
+		if !hasSolution {
+			result = currCoord
+			break
+		}
+
+		lineCount++
+	}
+
+	return fmt.Sprintf("%v,%v", result.col, result.row)
 }
